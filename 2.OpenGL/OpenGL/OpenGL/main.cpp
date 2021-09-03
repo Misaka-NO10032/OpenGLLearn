6edf8a13eb145c39dd3af5dcc8a617141faedab0
Misaka <1569451105@qq.com>
2021-09-03 09:51:51 +0800

    2.OpenGL add
    Co-Authored-By: Misha Gorodnitzky <misaka@pobox.com>

---
+++ 2.OpenGL/OpenGL/OpenGL/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenGL
+//
+//  Created by Misaka on 2019/9/6.
+//  Copyright © 2019 Misaka. All rights reserved.
+//
+
+#include <stdio.h>
+#include "GLShaderManager.h"
+#include "GLTools.h"
+#include <GLUT/GLUT.h>
+
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
